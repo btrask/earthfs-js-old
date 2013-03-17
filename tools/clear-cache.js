aab09e24c922f70c77d7f0cc19e63d66d7b18580
Ben Trask <bentrask@comcast.net>
2013-03-17 03:37:19 -0400

    Get clear-cache tool working again.

--- tools/clear-cache.js
+++ tools/clear-cache.js
@@ -18,4 +18,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE. */
 var fs = require("../server/utilities/fsx");
-fs.rmRecursive(__dirname+"/../cache");
+var shared = require("../server/shared");
+fs.rmRecursive(shared.CACHE, function(err) {
+	if(err && "ENOENT" !== err.code) console.log(err);
+});
